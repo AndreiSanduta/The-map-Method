b8c4d346f0227eb19e141ed60a2119a2d313d879
AndreiSanduta <andreisanduta@hotmail.com>
2023-02-02 20:35:23 +0000

    finish the exercie

--- map.js
+++ map.js
@@ -1,20 +1,33 @@
   
-/**
- * To run this file in Gitpod, use the 
- * command node map.js in the terminal
- */
+// /**
+//  * To run this file in Gitpod, use the 
+//  * command node map.js in the terminal
+//  */
 
-// Using a for loop
+// // Using a for loop
+// let nums = [1, 2, 3, 4, 5];
+// let results = [];
+// for (let num of nums) {
+//   results.push(num * 2);
+// }
+// console.log(results);
+
+// // Using map()
+// const multiplybyTwo = function (num) {
+//   return num * 6
+// }
+
+// const mapResoults = nums.map(multiplybyTwo);
+// console.log(mapResoults);
+
+// // Simplified w/ map()
+
+// const simplfied = nums.map(function(num) {return num * 3});
+// console.log(simplfied);
 
 
-// Using map()
-
-
-// Simplified w/ map()
-
-
-// Simplfied w/ map() + arrow function
-
+// // Simplfied w/ map() + arrow function
+c
 
 // With objects:
 const students = [
@@ -37,3 +50,10 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+
+const studentsWithId = students.map(student => [student.name, student.id]);
+console.log(studentsWithId);
+
+const studentWithH = students.map(students => students + 'h');
+ console.log(studentWithH);
